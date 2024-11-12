6dc789bbc87455d243ce4a4d5ccd7394306c5369
mark@xocean.com <mark@xocean.com>
2024-11-12 12:40:34 +0000

    run put one

--- ssds/index_all.sh
+++ ssds/index_all.sh
@@ -11,5 +11,18 @@ for (( i=o; i<$ns; i++ )); do
 	target="/mnt/usb_drives/ssd_${this_ssd}"
 	db="/opt/duc/dbs/full_ssd_${this_ssd}.${td}.db"
 	printf "Running on: %s for: %s,  DB: %s, Scanning: %s\n" "$td" "$this_ssd" "$db" "$target"
-	/usr/bin/duc index -x --uncompressed -d ${db} ${target}
+	# /usr/bin/duc index -x --uncompressed -d ${db} ${target}
+
+
+	jq_string='.blockdevices[] | select (.subsystems=="block:scsi:usb:pci") | select (.children[0].label | contains("SSD_NUM"))'
+	jq_string=$(echo $jq_string | sed "s/SSD_NUM/${this_ssd}/g")
+	printf "Jq string: %s\n" "$jq_string"
+	json_fn="ssd_${this_ssd}.${td}.json"
+	json_fil="/var/log/xocean_data_centre/inventories/${json_fn}"
+	s3_fil="s3://xocean-production-raw-dc-eu-west-2/DC1/ssd_${this_ssd}/device_info/${json_fn}"
+	/opt/xo_dc/ssds/ssds -j | jq -r "$jq_string" > "${json_fil}"
+
+	printf "Uploading file from: %s to %s\n" "$json_fil" "$s3_fil"
+	aws --profile dc_auto_camera s3 cp  "${json_fil}" "${s3_fil}"
+
 done	
--- ssds/run_put_one.sh
+++ ssds/run_put_one.sh
@@ -1,7 +1,67 @@
 #!/bin/bash
+max_procs=2
 ssd=$1
+tail_this=$2
+sdir=/opt/xo_dc/ssds
+
+
+if [[ "${#ssd}" -le 2 ]]; then
+	printf "Usage: %s 499\n" "$0"
+	ps -ef | grep -i ./put_one.sh | grep -v grep
+	nprocs=$(ps -ef | grep -i ./put_one.sh | grep -v grep | wc -l)
+	printf "%d instances running\n" "$nprocs"
+
+	if [[ "${ssd}" == "go" ]]; then
+	      	if [[ "${nprocs}" -ge "$max_procs" ]]; then
+			printf "Too many uploads running ( %d running ).  Wait till current batch finish or manually override.\n" "$nprocs"
+			exit 0
+		fi
+
+		printf "Running in auto mode.\n"
+		tf=$(mktemp)
+		/opt/xo_dc/ssds/ssds -q -l > "$tf"
+
+		readarray -t ssds <<< $(cat "$tf")
+		ns="${#ssds[@]}"
+
+		for (( i=o; i<$ns; i++ )); do
+			nprocs=$(ps -ef | grep -i ./put_one.sh | grep -v grep | wc -l)
+
+	      		if [[ "${nprocs}" -gt "$max_procs" ]]; then
+				printf "Too many uploads running ( %d running ).  Wait till current batch finish or manually override.\n" "$nprocs"
+				exit 0
+			fi
+
+		        this_ssd="${ssds[$i]}"
+        		printf "Running for: %s\n" "$this_ssd" 
+
+			ps -ef | grep -i ./put_one.sh | grep -v grep | grep "put_one.sh ${this_ssd}"
+			result=$?
+
+			if [[ "$result" -eq 0 ]]; then 
+				printf "Already running for this ssd: %s\n" "$this_ssd"
+				continue
+			else
+				printf "Not lready running for this ssd: %s\nGoing to run now...\n\n" "$this_ssd"
+				printf "%s %s\n" "${sdir}/run_put_one.sh" "$this_ssd"
+				"${sdir}/run_put_one.sh" "$this_ssd"
+			fi
+					
+			sleep 1
+			printf "%d instances running\n" "$nprocs"
+			ps -ef | grep -i ./put_one.sh | grep -v grep
+		done
+
+		rm "$tf"
+	fi
+
+	exit 0
+fi
+
 log_fn="/var/log/xocean_data_centre/put_one_${ssd}.log"
 /opt/xo_dc/ssds/put_one.sh "$ssd" >> "$log_fn" 2>&1 &
 run_pid=$!
 
-/usr/bin/grc /usr/bin/tail -f "$log_fn" --pid="$run_pid"
+if [[ "$tail_this" == "tail" ]] || [[ "$tail_this" == 't' ]]; then
+	/usr/bin/grc /usr/bin/tail -f "$log_fn" --pid="$run_pid"
+fi
